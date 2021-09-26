58c4a0e222b56f6084e26f5b3e84e0f0e9716be5
Rohit sah <1904277@kiit.ac.in>
2021-09-26 21:30:52 +0530

    author routes commit

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(express.urlencoded({ limit: `10mb`, extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
@@ -24,6 +26,7 @@ db.on(`error`, (error) => console.error(error));
 db.once(`open`, () => console.log("CONNECTED TO MONGOOSE!"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("SERVER UP AND RUNNING ON PORT 3000...");
